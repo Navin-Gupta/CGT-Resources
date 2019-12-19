0c7af0492cbc3380926da4cac8e73d7984bbc1cc
Navin-Gupta <46860917+Navin-Gupta@users.noreply.github.com>
2019-12-19 15:03:56 +0530

    Add files via upload

--- SpringBase-Ann/src/com/cgt/spring/services/EmailService.java
+++ SpringBase-Ann/src/com/cgt/spring/services/EmailService.java
@@ -1,5 +1,10 @@
 package com.cgt.spring.services;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 // import org.springframework.stereotype.Component;
@@ -9,12 +14,25 @@ import com.cgt.spring.facade.IMessageService;
 
 
 @Component
+@Scope("prototype")
 public class EmailService implements IMessageService {
 
+	@Value("${email.sender}")
+	private String sender;
+	
+	@PostConstruct
+	public void initMethod() {
+		
+	}
+	
 	
 	@Override
 	public String sendMessage(String to, String message) {
 		return "Email send to : " + to + "[ " + message + " ]";
 	}
 	
+	@PreDestroy
+	public void destroyMethod() {
+		
+	}
 }
